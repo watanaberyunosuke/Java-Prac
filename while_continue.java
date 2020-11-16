4681a8313e4ae44446b4da75c28279b760567b3d
watanaberyunosuke <harrybwatson@protonmail.com>
2020-11-16 17:49:33 +1100

    New

---
+++ while_continue.java
@@ -0,0 +1,12 @@
+public class while_continue {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+                if (i == 4) {
+                    continue;
+                }
+        }
+    }
+}
